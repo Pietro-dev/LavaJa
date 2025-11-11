dd7ea9d53eda356b0fc1d7c6ac6d1b5ba480925b
Pietro-dev <pietro.pietromartins@gmail.com>
2025-11-11 18:19:56 -0300

    feat: configurações da geração de token e filter chain

---
+++ lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/infra/security/TokenService.java
@@ -0,0 +1,52 @@
+package io.github.pietro_dev.lavajaapi.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import io.github.pietro_dev.lavajaapi.model.Usuario;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Usuario usuario){
+        try {
+            Algorithm algoritmo = Algorithm.HMAC256(secret);
+            String token = JWT.create()
+                    .withIssuer("lavaja")
+                    .withSubject(usuario.getEmail())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algoritmo);
+
+            return token;
+        }catch (JWTCreationException exception){
+            throw new RuntimeException("Erro na geração do token", exception);
+        }
+    }
+
+    public String validateToken(String token){
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer("lavaja")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+
+        }catch (JWTVerificationException exception){
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate(){
+        return LocalDateTime.now().plusHours(6).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
