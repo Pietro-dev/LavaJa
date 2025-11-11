8bdf303aae088d18d36404ae4cecb4ac07e2bee3
Pietro-dev <pietro.pietromartins@gmail.com>
2025-11-11 12:24:44 -0300

    feat: adição de método para verificar se usuário existe pelo email

--- lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/UsuarioRepository.java
+++ lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/UsuarioRepository.java
@@ -2,6 +2,11 @@ package io.github.pietro_dev.lavajaapi.model.repository;
 
 import io.github.pietro_dev.lavajaapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    UserDetails findByEmail(String email);
 }
