61a3661365788d023ffcf6c5e8b4eb2cb0648b00
Pietro-dev <pietro.pietromartins@gmail.com>
2025-11-14 18:19:08 -0300

    feat: painel de dashboard para lava rapidos

--- lavaja-app/src/app/dashboard/page.tsx
+++ lavaja-app/src/app/dashboard/page.tsx
@@ -1,5 +1,5 @@
 // src/app/dashboard/page.tsx
-import { DashboardClient } from "components"
+import { DashboardClient } from "components/dashboard/lava-rapido/dashboardClient"
 
 // Esta página é Server Component, mas renderiza um Client Component
 export default function DashboardPage() {
