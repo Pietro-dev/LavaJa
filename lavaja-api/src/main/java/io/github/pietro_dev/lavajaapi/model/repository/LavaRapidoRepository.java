541f8763a86d9f56ceb01d48b660aabf879b29ea
Pietro-dev <pietro.pietromartins@gmail.com>
2025-11-14 18:06:13 -0300

    refactor: refatoração do método find by email

--- lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/LavaRapidoRepository.java
+++ lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/LavaRapidoRepository.java
@@ -1,8 +1,17 @@
 package io.github.pietro_dev.lavajaapi.model.repository;
 
 import io.github.pietro_dev.lavajaapi.model.LavaRapido;
+import io.github.pietro_dev.lavajaapi.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface LavaRapidoRepository extends JpaRepository<LavaRapido, Long> {
-    Boolean existsByEmail(String email);
+
+    LavaRapido findByEmail(String email);
+
+    Optional<LavaRapido> findLavaRapidoByEmail(String email);
+
+    boolean existsByEmail(String email);
 }
