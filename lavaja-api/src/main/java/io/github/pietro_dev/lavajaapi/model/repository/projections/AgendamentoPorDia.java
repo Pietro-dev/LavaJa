3d113a732d366cb208abd43fb0fdb27c6bfd32bb
Pietro-dev <pietro.pietromartins@gmail.com>
2025-11-14 18:13:56 -0300

    feat: projection para o dashboard

--- lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/projections/AgendamentoPorDia.java
+++ lavaja-api/src/main/java/io/github/pietro_dev/lavajaapi/model/repository/projections/AgendamentoPorDia.java
@@ -2,5 +2,5 @@ package io.github.pietro_dev.lavajaapi.model.repository.projections;
 
 public interface AgendamentoPorDia {
     Integer getDia();
-    Integer getTotalAgendamentos();
+    Long getTotalAgendamentos();
 }
